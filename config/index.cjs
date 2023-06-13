8c3727af8a937e68be8ebb99f0cf9b758584d8a4
Cedric Niu <58715113+Cedric0927@users.noreply.github.com>
2023-06-14 00:18:25 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -5,7 +5,7 @@
  * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
  * 自行添加属性，以支持更多个性化功能
  */
-const USER_CONFIG = {"APP_ID":"wx99fd9a9116bb7882","APP_SECRET":"d3e78df43ad569bd736adf5673d00c5e","IS_SHOW_COLOR":true,"CALLBACK_TEMPLATE_ID":"","CALLBACK_USERS":[{"name":"自己","id":""}],"USERS":[{"name":"宝贝","id":"ootVV6vE6emaWzWcA5cYDgK0em1g","useTemplateId":"rvChjsFqr9KtTCS9cqsuc9el9bdC1cAVWhAvjC-2zV8","province":"浙江","city":"台州","horoscopeDate":"06-13","horoscopeDateType":"今日","openUrl":"https://www.baidu.com/s?ie=UTF-8&wd=%E4%BB%8A%E5%A4%A9%E4%B9%9F%E8%A6%81%E5%BC%80%E5%BF%83%E5%91%80","festivals":[{"type":"生日","name":"生日","date":"06-13","year":"2023"}],"customizedDateList":[]}],"SWITCH":{"weather":true,"holidaytts":true,"CIBA":true,"oneTalk":true,"earthyLoveWords":false,"momentCopyrighting":false,"poisonChickenSoup":false,"poetry":false,"horoscope":true,"birthdayMessage":true}}
+const USER_CONFIG = {"APP_ID":"wx99fd9a9116bb7882","APP_SECRET":"d3e78df43ad569bd736adf5673d00c5e","IS_SHOW_COLOR":true,"CALLBACK_TEMPLATE_ID":"","CALLBACK_USERS":[{"name":"自己","id":""}],"USERS":[{"name":"宝贝","id":"ootVV6uVB8VPgXVmljXinqnnXdqI","useTemplateId":"rvChjsFqr9KtTCS9cqsuc9el9bdC1cAVWhAvjC-2zV8","province":"浙江","city":"台州","horoscopeDate":"06-13","horoscopeDateType":"今日","openUrl":"https://www.baidu.com/s?ie=UTF-8&wd=%E4%BB%8A%E5%A4%A9%E4%B9%9F%E8%A6%81%E5%BC%80%E5%BF%83%E5%91%80","festivals":[{"type":"生日","name":"生日","date":"06-13","year":"2023"}],"customizedDateList":[]}],"SWITCH":{"weather":true,"holidaytts":true,"CIBA":true,"oneTalk":true,"earthyLoveWords":false,"momentCopyrighting":false,"poisonChickenSoup":false,"poetry":false,"horoscope":true,"birthdayMessage":true}}
 
 module.exports = USER_CONFIG
 
